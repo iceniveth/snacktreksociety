2dacb67c1f318c46f868d19bb76b776763e38832
iceniveth <ken11zer01@gmail.com>
2019-08-17 07:26:11 +0800

    Create user on firestore after sign up

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const firestore = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +11,25 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.authUserCreated = functions.auth.user().onCreate((user) => {
+  const {
+    displayName,
+    email,
+    emailVerified,
+    phoneNumber,
+    photoURL,
+    uid,
+  } = user;
+
+  return firestore
+    .collection('users')
+    .doc(uid)
+    .set({
+      displayName,
+      email,
+      emailVerified,
+      phoneNumber,
+      photoURL,
+    });
+});
