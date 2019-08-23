e2ac2999b925a2cce5315a9e21f731d3e3e9c210
Jon Tristan D. Jeruta <jtjeruta.stacktrek@gmail.com>
2019-08-23 10:42:11 +0800

    promise randomiser

--- src/contexts/LuckyLoserContext.js
+++ src/contexts/LuckyLoserContext.js
@@ -17,14 +17,69 @@ const LuckyLoserContextProvider = props => {
     { id: 8, name: 'Ming' },
   ]);
 
-  const [checkedPeople, setCheckedPeople] = useState(people.map(p => ({ ...p, count: 0 })));
+  const [checkedPeople, setCheckedPeople] =
+    useState(people.map(p => ({ ...p, count: 0 })));
 
+  const [gameData, setGameData] = useState('');
+
+
+
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  function sleep(ms, string) {
+    return (
+      new Promise((resolve, reject) => {
+        let wait = setTimeout( async () => {
+          clearTimeout(wait);
+          if (string) {
+            setGameData(string);
+          }
+          resolve(string)
+        }, ms)
+      })
+    );
+  }
+
+  async function resetCheckedPeopleCount () {
+    const newPeople = await checkedPeople.map(p => ({
+      id: p.id,
+      name: p.name,
+      count: 0,
+    }));
+    await setCheckedPeople(newPeople);
+  }
+
+  const playGame = async () => {
+    await setCheckedPeople(await shuffle(checkedPeople));
+    setGameStarted(true);
+    let count = 0;
+    while (!checkedPeople.find(p => p.count >= 3)) {
+      let newPeople = [...checkedPeople];
+      const random = await Promise.all([
+        Math.floor(Math.random() * checkedPeople.length),
+        sleep(count === 0 ? 0 : 800),
+        sleep(count === 0 ? 800 : 1600, '3...'),
+        sleep(count === 0 ? 1600 : 2400, '2...'),
+        sleep(count === 0 ? 2400 : 3200, '1...'),
+      ]);
+      count += 1;
+      const randomInt = random[0];
+      newPeople[randomInt].count += 1;
+      await sleep(1000, newPeople[randomInt].name);
+      setCheckedPeople(newPeople);
+    }
+    setGameStarted(false);
+    await resetCheckedPeopleCount();
+  }
 
   return (
     <LuckyLoserContext.Provider
       value={{
         people, setPeople, checkedPeople, setCheckedPeople,
         curTab, setCurTab, gameStarted, setGameStarted,
+        playGame, gameData,
       }}
     >
       {props.children}
--- src/pages/Games/LuckyLoser/Randomizer.jsx
+++ src/pages/Games/LuckyLoser/Randomizer.jsx
@@ -1,39 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Grid, Button } from '@material-ui/core';
 import { LuckyLoserContext } from '../../../contexts/LuckyLoserContext';
 
 
 const Randomizer = () => {
-  const {
-    checkedPeople: people, setCheckedPeople: setPeople, gameStarted, setGameStarted,
-  } = useContext(LuckyLoserContext);
-  const [gameData, setGameData] = useState('');
-
-  function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-  }
-
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  const startGame = async () => {
-    // const newPeople = await shuffle(people);
-    // await setPeople(newPeople);
-    // await setGameData('')
-    setGameStarted(true);
-    await sleep(1000);
-    people[0].count = 1;
-    console.log(people[0]);
-    // while (!people.find(p => p.count >= 3)) {
-    //   const randomInt = Math.floor(Math.random() * people.length);
-    //   await sleep(3000);
-    //   setGameData(people[randomInt].name);
-    //   people[randomInt].count += 1;
-    //   console.log(people[randomInt]);
-    //   setPeople(people);
-    // }
-  }
+  const { playGame, gameStarted, gameData } = useContext(LuckyLoserContext);
 
   return (
     <React.Fragment>
@@ -45,20 +16,20 @@ const Randomizer = () => {
         direction="column"
         style={{ height: 400 }}
       >
+        <h1>{gameData}</h1>
         {
           !gameStarted && (
             <Grid item>
               <Button
                 variant="contained"
                 color="primary"
-                onClick={() => startGame()}
+                onClick={() => playGame()}
               >
                 Start
               </Button>
             </Grid>
           )
         }
-        <h1>{gameData}</h1>
       </Grid>
     </React.Fragment>
   );
